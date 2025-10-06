a61244c88ace68340f4c18dad5739c163cbb5d57
Karthik Balasubramanian <karthikbalasubramanian08@gmail.com>
2025-10-06 22:56:36 +0530

    add nvim conf

---
+++ nvim/.config/nvim/plugin/colorsheme.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000, -- ensures it loads last
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+        float = {
+          transparent = false, -- makes floating windows opaque
+          solid = true, -- enables solid background for floats
+          border = "rounded", -- optional: sets rounded borders, can use "single", "double", etc.
+        },
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          telescope = true,
+        },
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+}

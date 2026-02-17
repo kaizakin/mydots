d2c6700ebb2577862bc1a2ed65d412a216d307aa
karthik balasubramanian <karthikbalasubramanian08@gmail.com>
2026-02-18 01:55:07 +0530

    add vim maximizer plugin

--- nvim/.config/nvim/lua/karthik/lazy.lua
+++ nvim/.config/nvim/lua/karthik/lazy.lua
@@ -12,3 +12,13 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("karthik.plugins")
+
+require("lazy").setup("karthik.plugins", {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})

21f771933570b78c435841cf71b742f0002ae0f0
karthik balasubramanian <karthikbalasubramanian08@gmail.com>
2026-03-03 00:49:57 +0530

    add cursor dark colorscheme to nvim

--- nvim/.config/nvim/lua/karthik/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/karthik/plugins/colorscheme.lua
@@ -1,4 +1,13 @@
 return {
-
+	{
+		"ydkulks/cursor-dark.nvim",
+		priority = 1000,
+		config = function()
+			require("cursor-dark").setup({
+				style = "dark-midnight", -- This matches the "extra dark" Cursor vibe
+				transparent = false,
+			})
+			vim.cmd.colorscheme("cursor-dark")
+		end,
+	},
 }
-

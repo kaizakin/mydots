adcb8c87092dc03c4b6a2630a77cf1cd726d82f1
karthik balasubramanian <karthikbalasubramanian08@gmail.com>
2026-03-28 16:41:34 +0530

    toggleterm updates

--- nvim/.config/nvim/lua/karthik/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/karthik/plugins/toggleterm.lua
@@ -1,12 +1,27 @@
 return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
+	keys = {
+		-- Mapping for Normal Mode (n) and Terminal Mode (t)
+		{ [[<C-\>]], "<cmd>ToggleTerm<cr>", mode = { "n", "t" }, desc = "Toggle Terminal" },
+	},
 	config = function()
 		require("toggleterm").setup({
 			size = 20,
 			open_mapping = [[<c-\>]], -- This is your shortcut (Ctrl + \)
 			direction = "float", -- Can be 'float', 'horizontal', or 'vertical'
 			shade_terminals = true,
+			insert_mappings = true,
+			terminal_mappings = true,
+			float_opts = {
+				border = "curved",
+			},
+			winbar = {
+				enabled = false,
+				name_formatter = function(term) --  term: Terminal
+					return term.name
+				end,
+			},
 		})
 	end,
 }
